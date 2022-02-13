c741d25da83794452d3f3b6bfe3bc84dfd8d4e12
Flavio Vieira <flavioemerson1@outlook.com>
2022-02-13 14:04:40 -0300

    feito a configuração de temas do app

--- cuidapet_mobile/lib/app/app_widget.dart
+++ cuidapet_mobile/lib/app/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:cuida_pet_mobile/app/core/constants/route_constants.dart';
+import 'package:cuida_pet_mobile/app/core/ui/ui_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:asuka/asuka.dart' as asuka;
@@ -12,6 +13,8 @@ class AppWidget extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(390, 844),
       builder: () => MaterialApp(
+        theme: UiConfig.theme,
+        title: UiConfig.title,
         debugShowCheckedModeBanner: false,
         initialRoute: RouteConstants.AUTH_ROUTE, //parei em 04:16 da aula splash
         builder: asuka.builder,
